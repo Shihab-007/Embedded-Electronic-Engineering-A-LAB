9c3a4ce00e528ac8e7d12b63177fd90f8bc6ccd9
emirkanS <72192251+emirkanS@users.noreply.github.com>
2023-06-05 11:11:10 +0200

    Update C++ implementation attempt.cpp

--- Implementations with C/C++ implementation attempt.cpp
+++ Implementations with C/C++ implementation attempt.cpp
@@ -3,6 +3,8 @@
 #include <cstdlib>
 #include <ctime>
 #include <vector>
+#include<unistd.h>
+unsigned int microsecond = 1000000;
 
 #define N 6
 
@@ -11,19 +13,24 @@ int list[N+1];
 int len;
 int nmbr_events;
 int event;
+int random_car;
 
 class Car {
 private:
     int id;
     int priority;
+    int timing;
     std::string location;
     std::string action;
+    std::string status;
+    
 
 public:
     Car(int carId) {
         id = carId;
         priority = rand() % 3 + 1;
-
+        timing = 0;
+        status = "out of range";
         // Randomly assign location
         int locationIndex = rand() % 4;
         switch (locationIndex) {
@@ -61,6 +68,13 @@ public:
         std::cout << "Priority: " << priority << std::endl;
         std::cout << "Location: " << location << std::endl;
         std::cout << "Action: " << action << std::endl;
+    }
+    
+    void printCarStatus() const {
+        std::cout << "Car ID: " << id << std::endl;
+        std::cout << "timing: " << timing << std::endl;
+        std::cout << "priority: " << priority << std::endl;
+        std::cout << "status: " << status << std::endl;
     }
 
     int getID() const {
@@ -103,38 +117,56 @@ enum class State {
 int main() {
     // Initialize random seed
     srand(time(0));
+    spawnCar();
     
     State currentState = State::FREE;
-    
+    while(true)
+    {
     switch(currentState) {
         case State::FREE:
-            std::cout << "State = FREE." << std::endl;
-            
-            //possible events: A car spawns, A car approaches
-            
-            event = randomizer(2);
-            
-            if (event == 1) // Spawn car
-            {
-                spawnCar();
-            }
-            else if (event == 2)
-            {
-                std::cout << "State Change. car approaches" << std::endl;
-                currentState = State::OCCUPIED;
-            }
         
-            // Print information of all cars
-            for (const Car& car : cars) {
-                car.printCarInfo();
-                std::cout << std::endl;
+            while(true)
+            {
+                std::cout << "State = FREE." << std::endl;
+            
+                //possible events: A car spawns, A car approaches
+                
+                std::cout << cars.size() << std::ends;
+                
+                usleep(3*microsecond);
+                event = randomizer(2);
+                
+                // Print information of all cars
+                for (const Car& car : cars) {
+                    car.printCarInfo();
+                    std::cout << std::endl;
+                }
+                
+                if (event == 1) // Spawn car
+                {
+                    spawnCar();
+                }
+                else if (event == 2)
+                {   
+                    random_car = rand() % cars.size()+1;
+                    Car(random_car).printCarStatus();
+                    std::cout << "State Change. car approaches" << std::endl;
+                    currentState = State::OCCUPIED;
+                    
+                    
+                    
+                    break;
+                }
+            
+                
             }
+            
             
             
             break;
         case State::OCCUPIED:
             std::cout << "State = OCCUPIED." << std::endl;
-            
+            usleep(3*microsecond);
             
             
             break;
@@ -148,5 +180,5 @@ int main() {
             std::cout << "Invalid state." << std::endl;
     }
     
-    
+    }
 }
