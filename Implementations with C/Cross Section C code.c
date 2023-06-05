6161cbb1adac783e4e92c2e9e24bf5df0b28228d
emirkanS <72192251+emirkanS@users.noreply.github.com>
2023-06-05 02:39:07 +0200

    Update Cross Section C code.c

--- Implementations with C/Cross Section C code.c
+++ Implementations with C/Cross Section C code.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define N 6 //number of cars
 
+int car1,car2,car3,car4,car5,car6;
+
+bool c1,c2,c3,c4,c5,c6;
+
 int list[N+1];
 int len;
-
-int cars[n];
 
 int blocks[4]; //4 blocks in the cross section
 
@@ -24,9 +28,30 @@ int locD[N];
 
 int cango=0; //variable to check if car can cross or not
 
-void spawn()
-{
+int nmbr_events = 1000;
 
+int generateCarID() {
+    static int carCounter = 0;
+    return ++carCounter;
+}
+
+void spawn()
+{   
+    if (c1 ==false)
+    {
+        car
+    }
+}
+
+
+int randomizer(nmbr_events)
+{
+    int event;
+    time_t t;
+    srand((unsigned) time(&t));
+    event = rand() % nmbr_events;
+    printf("%d\n", event);
+    return event;
 }
 
 void dequeue()
@@ -45,9 +70,33 @@ void enqueue()
 {
 }
 
-int main()
-{ 
+typedef enum {
+    FREE,
+    OCCUPIED,
+    CHECKING
+} State;
 
+int state = 0;
+
+int main()
+{       
+    switch(state) {
+        case FREE:
+            printf("State = FREE.\n\n");
+            
+            
+            
+            
+            break;
+        case OCCUPIED:
+            printf("The state is OCCUPIED.\n");
+            break;
+        case CHECKING:
+            printf("The state is PROCESSING.\n");
+            break;
+        default:
+            printf("Invalid state.\n");
+    }
 
     return 0;
 }
